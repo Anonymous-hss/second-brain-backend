5cdf070ad590e975964f7b7f5fb16cc62129f26e
Anonymous-hss <harshalsawatkar24@gmail.com>
2025-01-16 15:12:34 +0530

    mongo

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { UserModel } from "./db";
 
 const app = express();
+app.use(express.json());
 
-app.post("/api/v1/signup", (req, res) => {});
+app.post("/api/v1/signup", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  await UserModel.create({ username: username, password: password });
+
+  res.json({
+    success: true,
+  });
+});
 
 app.post("/api/v1/signin", (req, res) => {});
 
